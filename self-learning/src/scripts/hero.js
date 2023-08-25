4e2da0ce2af76fa08b5796d5aed20dbd4e48588c
Dana Alami <danaalami@msn.com>
2023-08-25 13:25:53 +0100

    fixed nav and finished product componenet

--- self-learning/src/scripts/hero.js
+++ self-learning/src/scripts/hero.js
@@ -1,5 +1,10 @@
-const player = document.querySelector('.player');
-const video = document.querySelector('video');
+// const player = document.querySelector('.player');
+// const video = document.querySelector('video');
+// const iframe = document.querySelector('iframe-video');
+
+// import wires from "../assets/wires.mp4";
+// const iframe = document.querySelector('iframe-video');
+
 
 // function togglePlay() {
 //     if(video.paused) {
@@ -8,7 +13,32 @@ const video = document.querySelector('video');
 //     }
 // }
 
-if (typeof video.loop == 'boolean') {
-    video.loop = true;
-}
-video.play();
+// if (typeof video.loop == 'boolean') {
+//     video.loop = true;
+// }
+// video.play();
+ 
+
+// if (typeof iframe.loop === 'boolean') {
+//     iframe.loop = true;
+// }
+// iframe.play();
+// document.addEventListener('DOMContentLoaded', function() {
+    // iframe.onload = function() {
+    //     iframe.contentWindow.document.body.innerHTML = '<iframe src="wires.mp4" autoplay loop></iframe>';
+    // };
+
+
+
+
+    // function playIframeVideoOnLoop(overlayIframe) {
+    //     const iframe = document.getElementById(overlayIframe);
+    //     iframe.src = iframe.src + "&loop=1";
+    // }
+
+    // playIframeVideoOnLoop("overlayIframe"); 
+
+    window.playIframeVideoOnLoop = function(overlayIframe) {
+        const iframe = document.getElementById(overlayIframe);
+        iframe.src = iframe.src + "&loop=1";
+      };
